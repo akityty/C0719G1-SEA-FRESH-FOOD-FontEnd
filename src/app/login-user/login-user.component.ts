6d9baa57972b120ca39b9ad50ffe33fdb1d4c4b6
lengochuyen25 <huyenln.hvnh@gmail.com>
2019-11-13 09:35:27 +0700

    change template

---
+++ src/app/login-user/login-user.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {UserService} from '../service/user.service';
+
+@Component({
+  selector: 'app-login-user',
+  templateUrl: './login-user.component.html',
+  styleUrls: ['./login-user.component.css']
+})
+export class LoginUserComponent implements OnInit {
+  loginForm: FormGroup;
+  message: string;
+
+  constructor(private fb: FormBuilder, private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(6)]],
+    });
+  }
+
+  submit() {
+    if (this.loginForm.valid) {
+      this.userService.login(this.loginForm.get('email').value, this.loginForm.get('password').value).subscribe(next => {
+        this.message = 'Bạn đã đăng nhập thành công!';
+      }, error => {
+        if (error.valueOf().status === 400) {
+          this.message = 'Sai mật khẩu';
+        } else {
+          this.message = 'Không tìm thây tài khoản';
+        }
+      });
+    }
+  }
+}
