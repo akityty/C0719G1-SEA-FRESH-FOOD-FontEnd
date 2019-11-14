eb363d2b897f5e1b72143a79321a6ba22aebb047
anhhaday <nguyenha26051988@gmail.com>
2019-11-14 15:53:55 +0700

    add test decentralization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,25 @@ import {UserService} from './service/user.service';
 })
 export class AppComponent {
   title = 'login-logout';
+  message = '';
 
   constructor(private userService: UserService) {
   }
 
+  admin() {
+    this.userService.admin().subscribe(next => {
+        this.message = 'thành công!';
+      }, error => {
+        this.message = 'thất bại';
+      }
+    );
+  }
+  users() {
+    this.userService.users().subscribe(next => {
+        this.message = 'thành công!';
+      }, error => {
+        this.message = 'thất bại';
+      }
+    );
+  }
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {IUser} from '../interface/user.interface';
@@ -11,20 +11,30 @@ export class UserService {
   userOnline: IUser = {email: '', password: ''};
   private readonly API_URL = 'http://localhost:8080/api';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
+
   getUsers(count = 10): Observable<IUser[]> {
     return this.http.get<IUser[]>(this.API_URL).pipe(
       map(response => response.filter((post, i) => i < count))
     );
   }
-    getById(id: number): Observable<IUser> {
-      return this.http.get<IUser>(`${this.API_URL}/${id}`);
+
+  getById(id: number): Observable<IUser> {
+    return this.http.get<IUser>(`${this.API_URL}/${id}`);
   }
 
   createUser(user: Partial<IUser>): Observable<IUser> {
-    return this.http.post<IUser>(this.API_URL, user);
+    return this.http.post<IUser>(`${this.API_URL}/create/`, user);
   }
+
   login(email: string, password: string) {
-    return this.http.get(`${this.API_URL}/checkLogin?email=${email}&&password=${password}`);
+    return this.http.get(`${this.API_URL}/checkLogin/?email=${email}&&password=${password}`);
+  }
+  admin() {
+    return this.http.get('http://localhost:8080/api/test/admin');
+  }
+  users() {
+    return this.http.get('http://localhost:8080/user');
   }
 }
