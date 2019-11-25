594f960fe81ee8e22746f1d21844e2c3b8103ff6
anhhaday <nguyenha26051988@gmail.com>
2019-11-25 17:37:53 +0700

    add success auth

--- src/app/FRESH-FOOD/service/product/product-service.service.ts
+++ src/app/FRESH-FOOD/service/product/product-service.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Product} from '../../interface/product/product';
+import {CookieService} from 'ngx-cookie-service';
 
 
 @Injectable({
@@ -11,14 +12,17 @@ export class ProductServiceService {
   private API_URL = 'http://localhost:8080/product/';
 
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private cookieService: CookieService) {
   }
 
   listProduct(): Observable<Product[]> {
    return  this.http.get<Product[]>(`${this.API_URL}home`);
   }
   createProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(`${this.API_URL}add`, product);
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.cookieService.get('jwtToken')
+    });
+    return this.http.post<Product>(`${this.API_URL}add`, product,{headers});
   }
 }
 
